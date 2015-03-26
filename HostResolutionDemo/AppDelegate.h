658611ab9913bcb9162d703d6165e5bf9e36b808
Hao Chen <ruochenruofu@gmail.com>
2015-03-26 16:13:26 +0800

    Initial Commit

---
+++ HostResolutionDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HostResolutionDemo
+//
+//  Created by ChenHao on 15/3/26.
+//  Copyright (c) 2015年 ChenHao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HostResolutionDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HostResolutionDemo
+//
+//  Created by ChenHao on 15/3/26.
+//  Copyright (c) 2015年 ChenHao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
